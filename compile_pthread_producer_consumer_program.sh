4a876aa78fcf069db9274e9ab9acd06e10c77e72
amitchoudhary0523 <amitchoudhary0523@gmail.com>
2025-11-26 18:45:48 +0530

    Deleted file compile_pthread_producer_consumer_program.sh
    Deleted file compile_pthread_producer_consumer_program.sh

--- compile_pthread_producer_consumer_program.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-
-rm -f pthread_producer_consumer_program.out
-
-gcc -Wall -Werror -Wextra -Wundef -Wunreachable-code -Winit-self -Wparentheses -Wconversion -Wsign-conversion -Wsign-compare -Werror-implicit-function-declaration -Wmissing-prototypes -Wmissing-declarations -Wformat-security pthread_producer_consumer_program.c -o pthread_producer_consumer_program.out
-
